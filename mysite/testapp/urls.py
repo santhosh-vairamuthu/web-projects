c7dc9316c1ace9e18333dd53f9b5db749aadf437
santhosh-vairamuthu <santhosh22032004@gmail.com>
2024-01-15 18:18:41 +0000

    created django custom form

--- mysite/testapp/urls.py
+++ mysite/testapp/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("<int:id>", views.index, name = "index"),
     path("home", views.home, name = "home"),
-    path("list", views.list, name = "list"),
+    path("list/<int:id>", views.list, name = "list"),
     path("create", views.create, name = "create"),
 ]
--- mysite/testapp/views.py
+++ mysite/testapp/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect
 from .models import ToDoList, Items
 from .forms import CreateNewList
 
 def index(response, id):
     data = ToDoList.objects.get(id=id)
+    
+
     return render(response, "testapp/base.html", {"name" : data.name})
 
-def list(response):
-    data = ToDoList.objects.get(id=1)
-    return render(response, "testapp/list.html", {"data" : data})
+def list(response, id):
+    data = ToDoList.objects.get(id=id)
+    if response.method == "POST":
+        if response.POST.get("save") == "save":
+            for i in data.items_set.all():
+                if response.POST.get("c"+str(i.id)) == "clicked":
+                    i.complete = True 
+                else:
+                    i.complete = False
+                i.save()
+        elif response.POST.get("add_new") == "add_new":
+            if len(response.POST.get("new")) >= 2:
+                data.items_set.create(text=response.POST.get("new"))
+            return redirect("/list/" + str(id))
+    return render(response, "testapp/list.html", {"data": data})
+
 
 def create(response):
     if response.method == "POST":
