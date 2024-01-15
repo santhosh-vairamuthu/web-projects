8b120b372096cb546a9d4ce7cff998108d7ab224
santhosh-vairamuthu <santhosh22032004@gmail.com>
2024-01-15 17:12:30 +0000

    created django inbuilt form

--- mysite/testapp/views.py
+++ mysite/testapp/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Items
+from .forms import CreateNewList
 
-def index(response):
-    return render(response, "testapp/base.html", {"name" : "god"})
+def index(response, id):
+    data = ToDoList.objects.get(id=id)
+    return render(response, "testapp/base.html", {"name" : data.name})
 
 def list(response):
     data = ToDoList.objects.get(id=1)
     return render(response, "testapp/list.html", {"data" : data})
+
+def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name = n)
+            t.save()
+        return HttpResponseRedirect("/%i" % t.id)
+    else:
+        form = CreateNewList()
+    return render(response, "testapp/create.html", {"form" : form})
 
 def home(response):
     return render(response, "testapp/home.html", {})
