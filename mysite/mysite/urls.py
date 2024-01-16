ca23b72dff9931c33b69dafe370941aa4559211f
santhosh-vairamuthu <santhosh22032004@gmail.com>
2024-01-16 07:54:43 +0000

    created django inbuilt logout form

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from register import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', views.register, name="register"),
     path('', include("testapp.urls")),
     path('', include("django.contrib.auth.urls")),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
